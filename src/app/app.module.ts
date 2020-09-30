1696a581d3ed4ddf4bebc120d0f1793752b6fd1f
LeThiBichNga <70678712+LeThiBichNga@users.noreply.github.com>
2020-09-30 15:30:02 +0700

    u

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ShareComponent } from './user/share/share.component';
+import { HeaderComponent } from './user/share/header/header.component';
+import { FooterComponent } from './user/share/footer/footer.component';
+import { BannerComponent } from './user/share/banner/banner.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ShareComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
